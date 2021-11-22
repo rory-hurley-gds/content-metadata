531e7c71c8aea3c9fb6959196add9bc908f68a09
Rory Hurley <rory.hurley@digital.cabinet-office.gov.uk>
2021-11-22 12:07:14 +0000

    pytorch scripts and huggingface notebooks.

--- src/dataset.py
+++ src/dataset.py
@@ -19,35 +19,37 @@ class EntityDataset:
         target_pos = []
         target_tag = []
 
+        #for each index and sentence, s, in texts
         for i, s in enumerate(text):
-            inputs = config.TOKENIZER.encode(
-                s,
-                add_special_tokens=False
-            )
-            input_len = len(inputs)
-            ids.extend(inputs)
-            target_pos.extend([pos[i]] * input_len)
-            target_tag.extend([tags[i]] * input_len)
+            
+            inputs = config.TOKENIZER.encode(s, add_special_tokens=False) #encode the sentence using tokeniser in config to a list of ids(?)
 
-            ids = ids[:config.MAX_LEN - 2]
-            target_pos = target_pos[:config.MAX_LEN - 2]
-            target_tag = target_tag[:config.MAX_LEN - 2]
+            
+            input_len = len(inputs) #input length is length of tokenised sentence
+            ids.extend(inputs) #add tokens to ids list
+            target_pos.extend([pos[i]] * input_len) #make list of pos, add to target_pos list
+            target_tag.extend([tags[i]] * input_len) #make list of tags, add to target_pos list
 
-            ids = [101] + ids + [102]
-            target_pos = [0] + target_pos + [0]
-            target_tag = [0] + target_tag + [0]
+            ids = ids[:config.MAX_LEN - 2] #take all ids, except the last two (for the first and last special tokens)
+            target_pos = target_pos[:config.MAX_LEN - 2] #take all target_pos, except the last two (for the first and last special tokens)
+            target_tag = target_tag[:config.MAX_LEN - 2] #take all target_tag, except the last two (for the first and last special tokens)
 
-            mask = [1] * len(ids)
-            token_type_ids = [0] * len(ids)
+            ids = [101] + ids + [102] #add special token for start and end of sentences
+            target_pos = [0] + target_pos + [0] #add a 0 pos to the start and end of sentences
+            target_tag = [0] + target_tag + [0] #add a 0 tag to the start and end of sentences
+            
+            mask = [1] * len(ids) #activate an mask for every token
+            token_type_ids = [0] * len(ids) #0 token type for every token in the list
 
-            padding_len = config.MAX_LEN - len(ids)
+            padding_len = config.MAX_LEN - len(ids) #padding is length of max layer, minus the length of the array
 
-            ids = ids + ([0] * padding_len)
-            mask = mask + ([0] * padding_len)
-            token_type_ids = token_type_ids + ([0] * padding_len)
-            target_pos = target_pos + ([0] * padding_len)
-            target_tag = target_tag + ([0] * padding_len)
+            ids = ids + ([0] * padding_len) #fill rest of id's with 0's for padding
+            mask = mask + ([0] * padding_len) #fill rest of attention mask with 0's for padding
+            token_type_ids = token_type_ids + ([0] * padding_len) #fill rest of token_type's with 0's for padding
+            target_pos = target_pos + ([0] * padding_len) #fill rest of target_pos's with 0's for padding
+            target_tag = target_tag + ([0] * padding_len) #fill rest of target_tag's with 0's for padding
 
+            # return a dictionary of tensors for input into the model
             return {
                 "ids": torch.tensor(ids, dtype=torch.long),
                 "mask": torch.tensor(mask, dtype=torch.long),
